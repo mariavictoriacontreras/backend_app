d600ab184b9abec07aa21442577015aff13c205b
Magali Vivas <mvivas@frro.utn.edu.ar>
2025-07-14 22:40:31 -0300

    Progress on USER CRUD and database connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import usuarioRoutes from "./routes/usuarios";
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.json()); 
+
+app.get('/', (_req, res) => {
   res.send('Hola Mundo desde Node.js + TypeScript!');
 });
+
+
+app.get("/test", (_req, res) => {
+  res.send("Ruta de test OK");
+});
+app.use("/usuarios", usuarioRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
