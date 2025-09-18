39a769a5aeb829b4cf3585d4a2fdcf6f1ae7a5fd
MagaliVivas <vivasmagali.c@gmail.com>
2025-09-18 11:20:36 -0300

    add stable users version with working endpoints and frontend connection

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 
 export const createApp = async () => {
   const app = express();
-  app.use(express.json());
+  app.use(express.json()); 
   app.use(cors({
     origin: 'http://localhost:5173', 
     credentials: true,             
